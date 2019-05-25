96c9f7c661118091e4d2560f823c83c3d432bf2e
Dan Scarf <dscarf@gmail.com>
2019-05-24 22:14:11 -0700

    Adding selinux

--- cookbooks/install_node/recipes/default.rb
+++ cookbooks/install_node/recipes/default.rb
@@ -3,3 +3,9 @@
 # Recipe:: default
 #
 # Copyright:: 2019, The Authors, All Rights Reserved.
+
+puts("You can log node info #{node['foo']} from a recipe using 'Chef::Log'")
+
+selinux_state "SELinux Permissive" do
+    action :disabled
+  end
